884af0ca50a76cdadaec55522baaf8e1142ee7dd
Rahul-S-Ashok <rahulashok2005@gmail.com>
2025-11-09 14:58:51 +0530

    exploring get ,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("helloo from the server!");
+// app.use('/user',(req,res)=>{
+//     res.send("HAHHAHAHAHAH");
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Akshay",lastName:"Saini"});
+})
+
+app.post("/user",(req,res)=>{
+    res.send("DATA SAVED TO DATA BASE");
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("edit madtaa ediini");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted user successfully");
 })
 
 
 
-app.listen("3000", ()=>{
+app.listen("8080", ()=>{
     console.log("connected to server");
 })

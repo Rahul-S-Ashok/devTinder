fd9d119b58460625d33562fa7b4b3551619964bb
Rahul-S-Ashok <rahulashok2005@gmail.com>
2025-12-07 01:14:11 +0530

    added some changes

--- src/routes/user.js
+++ src/routes/user.js
@@ -92,7 +92,7 @@ userRouter.get("/feed",userAuth , async(req,res)=>{
           .skip(skip)
           .limit(limit);   //imp
  
-        res.send(users);
+        res.json({data: users});
        
     }catch(err){
         res.status(400).json({message: err.message});

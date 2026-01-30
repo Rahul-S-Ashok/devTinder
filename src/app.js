827ac10f9c780354165aecd50392440c54046998
Rahul-S-Ashok <rahulashok2005@gmail.com>
2026-01-30 18:52:44 +0530

    chat

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
+const initSocket = require("./utils/socket");
 
 const app = express();
+const server = http.createServer(app);
 
 require("./utils/cronjob");
 
@@ -15,30 +18,22 @@ app.use(
   })
 );
 
-// ✅ RAW body ONLY for Razorpay webhook
 app.use("/payment/webhook", express.raw({ type: "application/json" }));
-
-// ✅ Normal middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-// ✅ Routes
 app.use("/auth", require("./routes/auth"));
 app.use("/profile", require("./routes/profile"));
 app.use("/request", require("./routes/request"));
 app.use("/user", require("./routes/user"));
 app.use("/payment", require("./routes/payment"));
+app.use("/", require("./routes/chat"));
 
 const PORT = process.env.PORT || 8080;
 
-connectDB()
-  .then(() => {
-    console.log("✅ Database connected");
-    app.listen(PORT, () =>
-      console.log(`🚀 Server running on port ${PORT}`)
-    );
-  })
-  .catch((err) => {
-    console.error("❌ DB error:", err.message);
-    process.exit(1);
-  });
+connectDB().then(() => {
+  initSocket(server);
+  server.listen(PORT, () =>
+    console.log(`🚀 Server running on port ${PORT}`)
+  );
+});
